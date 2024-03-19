93b7e60d9aa6b8fac421162c91a75cb5a63fbeb4
iamtanmaygithub <163767901+iamtanmaygithub@users.noreply.github.com>
2024-03-19 23:13:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st 
 import joblib
- model_nb = joblib.load('spam-ham')
+
+model_nb = joblib.load('spam-ham')
+
 st.title('SPAM HAM CLASSIFIER')
 ip= st.text_input('Enter your text : ')
+
 op = model_nb.predict([ip])
 if st.button('PREDICT')
    st.title(op[0])
